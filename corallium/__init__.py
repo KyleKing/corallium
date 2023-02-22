3c8bc013baabd56e4cf1ce7daa532e02c4c5f81b
Kyle King <KyleKing@users.noreply.github.com>
2023-02-22 09:20:20 -0500

    bump: version 0.1.0rc0 → 0.1.0rc1

--- corallium/__init__.py
+++ corallium/__init__.py
@@ -1,6 +1,6 @@
 """corallium."""
 
-__version__ = '0.1.0rc0'
+__version__ = '0.1.0rc1'
 __pkg_name__ = 'corallium'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
