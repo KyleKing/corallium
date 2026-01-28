1d95ee7e765e419f1719dc64bfbee5d37d73e472
Kyle King <KyleKing@users.noreply.github.com>
2026-01-27 22:36:35 -0600

    bump: version 2.1.1 → 2.2.0

--- corallium/__init__.py
+++ corallium/__init__.py
@@ -2,7 +2,7 @@
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
-__version__ = '2.1.1'
+__version__ = '2.2.0'
 __pkg_name__ = 'corallium'  # noqa: RUF067
 
 configure_runtime_type_checking_mode()  # noqa: RUF067
