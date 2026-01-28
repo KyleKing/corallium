4fefd865b0853fd4c761875c41316b1a28c4059d
Kyle King <KyleKing@users.noreply.github.com>
2026-01-28 01:28:57 -0600

    build: upgrade to calcipy 6.0.0b2

--- corallium/__init__.py
+++ corallium/__init__.py
@@ -1,11 +1,12 @@
+# ruff: noqa: RUF067
 """corallium."""
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
 __version__ = '2.2.0'
-__pkg_name__ = 'corallium'  # noqa: RUF067
+__pkg_name__ = 'corallium'
 
-configure_runtime_type_checking_mode()  # noqa: RUF067
+configure_runtime_type_checking_mode()
 
 
 # == Above code must always be first ==
